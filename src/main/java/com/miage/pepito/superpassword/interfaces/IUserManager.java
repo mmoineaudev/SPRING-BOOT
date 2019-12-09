f2199550d5865022aeda6d8cd15863486952b02c
Maxime M <maxime.moineau@free.fr>
2019-12-09 15:58:30 +0100

    Quickfix UserManager can give admin rights

--- src/main/java/com/miage/pepito/superpassword/interfaces/IUserManager.java
+++ src/main/java/com/miage/pepito/superpassword/interfaces/IUserManager.java
@@ -1,6 +1,6 @@
 package com.miage.pepito.superpassword.interfaces;
 
 public interface IUserManager {
-    public void addUser(String id, String password);
+    public void addUser(String id, String password, boolean admin);
     public IUser getUserByName(String name);
 }
