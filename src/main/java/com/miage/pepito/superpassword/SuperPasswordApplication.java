9ff61df466e162a9dc11d8ae81796c45350c7b80
Robin Prette <robin.prette@gmail.com>
2019-12-09 14:44:56 +0100

    chore: init project

---
+++ src/main/java/com/miage/pepito/superpassword/SuperPasswordApplication.java
@@ -0,0 +1,13 @@
+package com.miage.pepito.superpassword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperPasswordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperPasswordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miage/pepito/superpassword/SuperPasswordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miage.pepito.superpassword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperPasswordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
