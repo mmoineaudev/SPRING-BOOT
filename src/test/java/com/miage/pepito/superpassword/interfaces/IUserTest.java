a6d7e7be59454f47a8fb17bd5c25b6570f0ab7f3
Maxime M <maxime.moineau@free.fr>
2019-12-09 15:37:28 +0100

    add userTest

--- src/test/java/com/miage/pepito/superpassword/interfaces/IUserTest.java
+++ src/test/java/com/miage/pepito/superpassword/interfaces/IUserTest.java
@@ -1,23 +1,85 @@
 package com.miage.pepito.superpassword.interfaces;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class IUserTest {
 
-    private IUser userOne, UserTwo, userAdmin;
+    private IUser userOne, userTwo, userAdmin;
+
+    @BeforeEach
+    public void init(){
+        userAdmin = new IUser() {
+            @Override
+            public boolean isAdmin() {
+                return true;
+            }
+
+            @Override
+            public String getId() {
+                return "userAdmin";
+            }
+
+            @Override
+            public String getPassword() {
+                return "admin123456";
+            }
+        } ;
+        userOne = new IUser() {
+            @Override
+            public boolean isAdmin() {
+                return false;
+            }
+
+            @Override
+            public String getId() {
+                return "userOne";
+            }
+
+            @Override
+            public String getPassword() {
+                return "userOnePassword";
+            }
+        };
+        userTwo = new IUser() {
+            @Override
+            public boolean isAdmin() {
+                return false;
+            }
+
+            @Override
+            public String getId() {
+                return "userTwo";
+            }
+
+            @Override
+            public String getPassword() {
+                return "userTwoPassword";
+            }
+        };
+
+    }
 
     @Test
     void isAdmin() {
-
+        assertTrue(userAdmin.isAdmin());
+        assertFalse(userOne.isAdmin());
+        assertFalse(userTwo.isAdmin());
     }
 
     @Test
     void getId() {
+        assertEquals("userAdmin",userAdmin.getId());
+        assertEquals("userOne",userOne.getId());
+        assertEquals("userTwo",userTwo.getId());
     }
 
     @Test
     void getPassword() {
+        assertEquals("admin123456",userAdmin.getPassword());
+        assertEquals("userOnePassword",userOne.getPassword());
+        assertEquals("userTwoPassword",userTwo.getPassword());
     }
 }
