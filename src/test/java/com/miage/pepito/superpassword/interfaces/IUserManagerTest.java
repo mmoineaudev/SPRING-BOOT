db8c9a58bc9fc52f92f830f231b7284189a54f02
Maxime M <maxime.moineau@free.fr>
2019-12-09 15:46:38 +0100

    Add UserManagerTest

--- src/test/java/com/miage/pepito/superpassword/interfaces/IUserManagerTest.java
+++ src/test/java/com/miage/pepito/superpassword/interfaces/IUserManagerTest.java
@@ -1,16 +1,21 @@
 package com.miage.pepito.superpassword.interfaces;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class IUserManagerTest {
-
-    @Test
+    IUserManager userManager;
+    @BeforeEach
     void addUser() {
+        userManager.addUser("Jean", "passwordJean");
+        userManager.addUser("Jean2", "passwordJean2");
     }
 
     @Test
     void getUserByName() {
+        assertEquals("passwordJean",userManager.getUserByName("Jean").getPassword());
+        assertEquals("passwordJean2",userManager.getUserByName("Jean2").getPassword());
     }
 }
